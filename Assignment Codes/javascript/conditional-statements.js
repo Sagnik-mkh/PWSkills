8bcc573eb7aee5a5e6fa17093041359219d0657f
Sagnik Mukhopadhyay <sagnikm003@gmail.com>
2025-08-02 11:13:24 +0530

    Add files via upload

---
+++ Assignment Codes/javascript/conditional-statements.js
@@ -0,0 +1,10 @@
+// Conditional Statements
+let age = 21;
+
+if (age >= 18) {
+    //executes when true
+    console.log("Can drive");
+} else {
+    //executes when other conditions are false
+    console.log("Cannot drive");
+}
