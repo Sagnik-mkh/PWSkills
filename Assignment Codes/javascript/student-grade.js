dff3dc4ecfb3675cfe826d204e6c7bd649cecd42
Sagnik Mukhopadhyay <sagnikm003@gmail.com>
2025-08-02 11:16:47 +0530

    Add files via upload

---
+++ Assignment Codes/javascript/student-grade.js
@@ -0,0 +1,6 @@
+let marks = 80; //initialized marks
+
+if (marks > 90) console.log("A"); //grade A when marks > 90
+else if (marks > 70) console.log("B"); // grade B when marks > 70 <= 90
+else if (marks > 50) console.log("C"); // grade C when marks > 50 <= 70
+else console.log("F"); // if none satisfied then F
