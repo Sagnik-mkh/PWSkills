f4a7257e4b4dea3b940ad7c88e5baf93db7df48a
Sagnik Mukhopadhyay <sagnikm003@gmail.com>
2025-08-02 15:54:07 +0530

    Add files via upload

---
+++ Assignment Codes/javascript/greetings.js
@@ -0,0 +1,10 @@
+//function to greet
+function greet(name) {
+    console.log("Good Morning,", name);
+}
+
+let arr = ["Asuna", "Liz", "Kirito"];
+
+for (let i = 0; i < arr.length; i++) {
+    greet(arr[i]);
+}
